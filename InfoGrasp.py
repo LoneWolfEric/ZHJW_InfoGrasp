325b1f77fe1c47085f3aa13a5e98918b5ca832e5
LoneWolfEric <871855626@qq.com>
2019-12-04 14:51:43 +0800

    selenium can get the infomation I want

--- InfoGrasp.py
+++ InfoGrasp.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+from PIL import Image
 from bs4 import BeautifulSoup
 import hashlib
 import time
+
+
+# 创建一个类用于存储学生信息
+class StudentInfo(object):
+    name = ''
+    student_id = ''
+    major = ''
+    college = ''
+    degree = ''
+    grade = ''
+    gender = ''
+    native_place = ''
+    email = ''
+    phone = ''
 
 class Spider(object):
 
@@ -14,23 +29,72 @@ class Spider(object):
 
     # 访问登陆页面，获取下载验证码
     def get_CAPTCHA(self):
-        pass
+        self.driver.get('http://zhjw.scu.edu.cn/login')
+        time.sleep(2)
+        self.driver.maximize_window()
+        self.driver.save_screenshot('screenshot.png')
+        captcha = self.driver.find_element_by_id('captchaImg')
+        location = captcha.location
+        size = captcha.size
+        print(captcha.location)
+        print(captcha.size)
+        left = location['x']
+        top = location['y']
+        right = left + size['width']
+        bottom = top + size['height']
+
+        img=Image.open('screenshot.png')
+        img = img.crop((left,top,right,bottom))
+        img.save('captch.png')
+        # print(self.driver.page_source) 
 
     # 输入学号密码验证码，登陆
     def login(self):
+        username_box = self.driver.find_element_by_id('input_username')
+        password_box = self.driver.find_element_by_id('input_password')
+        captch_box = self.driver.find_element_by_id('input_checkcode')
+
+        username_box.send_keys('2016141442100')
+        password_box.send_keys('lyx-19980708')
+        captch = str(input('captch?'))
+        captch_box.send_keys(captch)
+        self.driver.save_screenshot('filltheform.png')
+
+        submit_bottom = self.driver.find_element_by_id('loginButton')
+        submit_bottom.click()
+        time.sleep(1)
+        self.driver.save_screenshot('login.png')
         pass
 
     # 进入登陆页面，获取个人信息
     def get_personl_info(self):
-        pass
+        # 进入个人信息界面
+        # menu-text = self.driver.find_element_by_id('1181690')
+        time.sleep(3)
+        self.driver.get('http://zhjw.scu.edu.cn/student/rollManagement/rollInfo/index')
+        self.driver.save_screenshot('personalinfo.png')
+
+        with open('prsonalinfo.html', 'wb') as f:
+            f.write(self.driver.page_source.encode('utf-8'))
+
+        # print(self.driver.page_source)
+        student = StudentInfo()
+
+        student.name = ''
+        student.student_id = ''
+        student.major = ''
+        student.college = ''
+        student.degree = ''
+        student.grade = ''
+        student.gender = ''
+        student.native_place = ''
+        student.email = ''
+        student.phone = ''
 
     # 断开连接
     def logout(self):
-
-
-
-        client.quit()
-        pass
+        self.driver.delete_all_cookies()
+        self.driver.quit()
 
     def run(self):
         self.get_CAPTCHA()
